c6d5421668ac47b6c275aee17870dae21231e184
Munki-Jeong <jmunki1004@gmail.com>
2023-05-06 01:11:00 +0900

    commit

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 import routers as article_router
-from . import models, database
+import models, database
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+app.include_router(article_router.router, prefix="/article", tags=["article"])
 
-@app.get("/")
-async def root():
-    app.include_router(article_router.router, prefix="/article", tags=["article"])
 
 # To create the initial SQLite database, run the following lines after running the server for the first time:
 # from . import models, database
