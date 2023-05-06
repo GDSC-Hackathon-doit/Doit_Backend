6a36190ca930554d4c8752ac4bfcf430f1e08a7a
Munki-Jeong <60166763+Munki-Jeong@users.noreply.github.com>
2023-05-06 12:49:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ app = FastAPI()
 app.include_router(routers.router, prefix="/article", tags=["article"])
 
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to my API!"}
-
-
-
-
 # To create the initial SQLite database, run the following lines after running the server for the first time:
 # from . import models, database
 # models.Base.metadata.create_all(bind=database.engine)
